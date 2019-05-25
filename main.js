16f822ace8cb04167ad1b6bb6c3eb15fd5881520
vitalii <dumavb777@gmail.com>
2019-05-25 18:10:10 +0300

    sol

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
 const table = document.getElementById('table');
 
-const sortNumber = (a ,b) => a - b;
-
-const sortString = (a ,b) => a.localeCompare(b);
-
-function SortTr(event) {
+function sortTr(event) {
     const { rows } = table.tBodies[0];
     const { cellIndex } = event.target;
-    const { dataset } = event.target;
-    // console.log(dataset)
-    // const sortCells =
+    const { type } = event.target.dataset;
+    const body = table.tBodies[0];
     let newRowsTable = [];
+
     for (let row of rows) {
-        newRowsTable = [...newRowsTable,row];
+        newRowsTable = [...newRowsTable, row];
     }
 
-    if (dataset === 'number') {
-        newRowsTable.sort((a, b) => a.cells[cellIndex].innerText - b.cells[cellIndex].innerText);
-    } else if (dataset === 'string') {
-        newRowsTable.sort((a, b) => a.cells[cellIndex].innerText.localeCompare(b.cells[cellIndex].innerText));
+    if (type === 'number') {
+        newRowsTable.sort((rowFirst, rowSecond) => rowFirst.cells[cellIndex].textContent - rowSecond.cells[cellIndex].textContent);
+    } else if (type === 'string') {
+        newRowsTable.sort((rowFirst, rowSecond) => rowFirst.cells[cellIndex].textContent.localeCompare(rowSecond.cells[cellIndex].textContent));
     }
-    newRowsTable.forEach(a => table.tBodies[0].appendChild(a ));
 
-     //    newRowsTable.sort((a, b)=>{return a.cells[cellIndex].textContent - b.cells[cellIndex].textContent})
-     // newRowsTable.forEach(a => console.log(a.cells[cellIndex].textContent));
-     //    console.log(newRowsTable)
+    newRowsTable.forEach(row => body.appendChild(row));
 }
 
-table.tHead.addEventListener('click', SortTr)
-
-
-
-
-// const table = document.getElementById('table');
-//
-// function sortRows(event) {
-//     const rows = table.rows;
-//     const dataAttribute = event.target.getAttribute('data-type');
-//     const newRowsTable = [];
-//     for (let i = 1; i < rows.length; i++) {
-//         newRowsTable.push(rows[i]);
-//     }
-//
-//     if (dataAttribute === 'number') {
-//         newRowsTable.sort((a, b) => a.cells[0].innerText - b.cells[0].innerText);
-//     } else if (dataAttribute === 'string') {
-//         newRowsTable.sort((a, b) => a.cells[1].innerText.localeCompare(b.cells[1].innerText));
-//     }
-//     newRowsTable.forEach(a => table.tBodies[0].appendChild(a));
-// }
-//
-// table.addEventListener('click', sortRows);
+table.tHead.addEventListener('click', sortTr);
