b1ef1c9a1e8e6d26140c781dc5ff723dc9d6591b
vitalii <dumavb777@gmail.com>
2019-05-25 17:50:08 +0300

    sol

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 const table = document.getElementById('table');
 
-function sortRows(event) {
-    const rows = table.rows;
-    const dataAttribute = event.target.getAttribute('data-type');
-    const newRowsTable = [];
-    for (let i = 1; i < rows.length; i++) {
-        newRowsTable.push(rows[i]);
+const sortNumber = (a ,b) => a - b;
+
+const sortString = (a ,b) => a.localeCompare(b);
+
+function SortTr(event) {
+    const { rows } = table.tBodies[0];
+    const { cellIndex } = event.target;
+    const { dataset } = event.target;
+    // console.log(dataset)
+    // const sortCells =
+    let newRowsTable = [];
+    for (let row of rows) {
+        newRowsTable = [...newRowsTable,row];
     }
 
-    if (dataAttribute === 'number') {
-        newRowsTable.sort((a, b) => a.cells[0].innerText - b.cells[0].innerText);
-    } else if (dataAttribute === 'string') {
-        newRowsTable.sort((a, b) => a.cells[1].innerText.localeCompare(b.cells[1].innerText));
+    if (dataset === 'number') {
+        newRowsTable.sort((a, b) => a.cells[cellIndex].innerText - b.cells[cellIndex].innerText);
+    } else if (dataset === 'string') {
+        newRowsTable.sort((a, b) => a.cells[cellIndex].innerText.localeCompare(b.cells[cellIndex].innerText));
     }
-    newRowsTable.forEach(a => table.tBodies[0].appendChild(a));
+    newRowsTable.forEach(a => table.tBodies[0].appendChild(a ));
+
+     //    newRowsTable.sort((a, b)=>{return a.cells[cellIndex].textContent - b.cells[cellIndex].textContent})
+     // newRowsTable.forEach(a => console.log(a.cells[cellIndex].textContent));
+     //    console.log(newRowsTable)
 }
 
-table.addEventListener('click', sortRows);
+table.tHead.addEventListener('click', SortTr)
+
+
+
+
+// const table = document.getElementById('table');
+//
+// function sortRows(event) {
+//     const rows = table.rows;
+//     const dataAttribute = event.target.getAttribute('data-type');
+//     const newRowsTable = [];
+//     for (let i = 1; i < rows.length; i++) {
+//         newRowsTable.push(rows[i]);
+//     }
+//
+//     if (dataAttribute === 'number') {
+//         newRowsTable.sort((a, b) => a.cells[0].innerText - b.cells[0].innerText);
+//     } else if (dataAttribute === 'string') {
+//         newRowsTable.sort((a, b) => a.cells[1].innerText.localeCompare(b.cells[1].innerText));
+//     }
+//     newRowsTable.forEach(a => table.tBodies[0].appendChild(a));
+// }
+//
+// table.addEventListener('click', sortRows);
